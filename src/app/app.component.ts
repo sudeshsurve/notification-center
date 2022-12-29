44f7e6f9bda66f5bf4ca23ebe243f63bd6b180b3
Your Name <sudesh@gmail.com>
2022-12-29 10:33:21 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NzButtonSize } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('#email') email :any
+ value :any= {
+email : ""
+ }
+   
+
+  listOfData = [
+    {
+      email: 'John Brown',   
+    },
+    {
+      email: 'Jim Green',
+    },
+    {
+      email: 'sudesh@gmail.com ',
+    },
+    {
+      email: 'sammer@gmail.com',
+    },
+  ]
   title = 'notification-center';
+  isVisible = false;
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+
+  submit(): void {
+    console.log('Button ok clicked!');
+    console.log(this.email);
+    
+   this.listOfData.push({email : this.value.email})
+    this.isVisible = false;
+  
+  }
+
+  deleteemail(i:any){
+    console.log(i);
+  this.listOfData.splice(i , 1)
+  }
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzTableModule } from 'ng-zorro-antd/table'
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +26,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzInputModule,
+    NzGridModule,
+    NzCardModule,
+    NzButtonModule,
+    NzIconModule,
+    NzModalModule,
+    NzListModule,
+    NzTableModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
